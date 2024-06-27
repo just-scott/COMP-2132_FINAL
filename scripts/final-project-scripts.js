f275de2e76243a622f79aeb1d075d9ff065281f0
just-scott <scott.tatarchuk@gmail.com>
2024-06-27 14:26:56 -0700

    added method to get a computer name and started the setupGame function

--- scripts/final-project-scripts.js
+++ scripts/final-project-scripts.js
@@ -1,1 +1,15 @@
 const computerPlayerNames = ['DB Cooper', 'Wild Bill', 'El Matador', 'Devilish Dan', 'Al Capone', 'Don Juan'];
+
+let playerName = prompt('Enter your name to play: ');
+
+
+function setupGame()
+{
+    let player = new Player(playerName);
+    let computer = new Player(getComputerName());
+}
+
+function getComputerName()
+{
+    return computerPlayerNames[Math.floor(Math.random() * computerPlayerNames.length) + 1];
+}
