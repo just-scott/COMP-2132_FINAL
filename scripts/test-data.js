eb209703a7846f272b84e542a51b5446b611fb81
just-scott <scott.tatarchuk@gmail.com>
2024-06-27 16:23:37 -0700

    got die images to display

--- scripts/test-data.js
+++ scripts/test-data.js
@@ -2,3 +2,5 @@
 //TODO delete this file before submitting the project
 
 $playerName.text(playerName);
+$playerFirstRoll.children(':nth-child(1)').attr('src', 'images/die-face-1.png');
+$playerFirstRoll.children(':nth-child(2)').attr('src', 'images/die-face-5.png');
